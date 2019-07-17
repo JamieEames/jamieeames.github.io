e515bffc11d1e928d7f3b341e810d25901855f5c
jamiee7 <52979683+jamiee7@users.noreply.github.com>
2019-07-18 00:57:01 +0100

    Expanded funtionality and Reduced code
    Introduced For Loops to create agents and control movement and boundary effect handling.
    
    Number of agents and number of itterations, now controlled with  variable.s
    
    The loops remove a significant amount of redundant repeated code.
    
    Boundary effect now resolved with torus method.
    
    For loop in chart block, will now plot however many agents are in the model.
    
    Pythagorian distance and most eastern agents blocks are currently inactive.

--- ABM.py
+++ ABM.py
@@ -2,87 +2,49 @@ import random
 import operator
 import matplotlib.pyplot
 
+#Number of Agents Variable
+num_of_agents = 10
+num_of_itterations = 100
+
 #Agents List
 agents = []
 
-agents.append([random.randint(0,99),random.randint(0,99)]) #adds x0 and y0 to agents
 
+#Generates a number of agents in random locations
+for i in range(num_of_agents):
+    agents.append([random.randint(0,99),random.randint(0,99)])  
 
-#Generates a ramdon number and checks the value
-#if less than 0.5, 1 is added to y0
-#otherwise 1 is subtracted from y0
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[0][0] += 1
-else:
-    agents[0][0] -=1
-
-#above codeblock repeated for x0
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[0][1] += 1
-else:
-    agents[0][1] -=1
-
-#repeat step
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[0][0] += 1
-else:
-    agents[0][0] -=1
-
-#above codeblock repeated for x0
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[0][1] += 1
-else:
-    agents[0][1] -=1
-
-#creates variables y0/x0
-#and assigns ramdom start values between 0 and 99
-agents.append([random.randint(0,99),random.randint(0,99)]) #adds x0 and y0 to agents
-
-#Generates a ramdon number and checks the value
-#if less than 0.5, 1 is added to y1
-#otherwise 1 is subtracted from y1
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[1][0] += 1
-else:
-    agents[1][0] -=1
-
-#above codeblock repeated for x0
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[1][1] += 1
-else:
-    agents[1][1] -=1
-
-#repeat step
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[1][0] += 1
-else:
-    agents[1][0] -=1
-
-#above codeblock repeated for x0
-if random.random() < 0.5: #random.random() replaces ramdon_number
-    agents[1][1] += 1
-else:
-    agents[1][1] -=1
-    
-print(agents)
+#Moves each agent a number of times
+for i in range(num_of_agents):
+    for j in range(num_of_itterations): 
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
 
 #Calculate distance between xy0/xy1
-distance = (((agents[0][0]-agents[1][0])**2) + ((agents[0][1]-agents[1][1])**2))**0.5
-print(distance)
+##distance = (((agents[0][0]-agents[1][0])**2) + ((agents[0][1]-agents[1][1])**2))**0.5
+##print(distance)
 
 #Print the coord pair with the highest index 1 (X-Coord east) value
 
 ###
-eastmost = max(agents, key=operator.itemgetter(1))
+##eastmost = max(agents, key=operator.itemgetter(1))
 ###########All below may need altering if the tutor says so, then revert to plotting each point.#########
-noteastmost = agents
-del noteastmost[noteastmost.index(eastmost)]
-notestmostsingle = noteastmost[0]
+##noteastmost = agents
+##del noteastmost[noteastmost.index(eastmost)]
+##notestmostsingle = noteastmost[0]
 
 
 ###Plots points, with the most Eastern in Red
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(notestmostsingle[1],notestmostsingle[0])
-matplotlib.pyplot.scatter(eastmost[1], eastmost[0], color='red')
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+##matplotlib.pyplot.scatter(notestmostsingle[1],notestmostsingle[0])
+##matplotlib.pyplot.scatter(eastmost[1], eastmost[0], color='red')
 matplotlib.pyplot.show()

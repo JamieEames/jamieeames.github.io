e2aff0a46f5d89cbc972bd05e9dbf4255351b0a3
jamiee7 <52979683+jamiee7@users.noreply.github.com>
2019-07-28 20:14:25 +0100

    Full environment usage
    Added functionality to make agents use an area to size of the enviroment file. This includes starting location, movement and proper edge handling. Alters plot scales accordingly.
    
    Agent start location randoniser code moved into ABM module.
    
    Updated comments throughout.

--- ABM.py
+++ ABM.py
@@ -1,18 +1,17 @@
 import random
-import operator
 import matplotlib.pyplot
 import agentframework
 import csv
 
+#Sets up the function to calulate the distance between each agent
 def distance_between(agents_row_a, agents_row_b):
         return (((agents_row_a.x - agents_row_b.x)**2)\
                 + ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
+#Reads in an environment csv file, and transforms it into a 2D list
 data = open('in.txt', newline='')
 dataset = csv.reader(data, quoting=csv.QUOTE_NONNUMERIC)
-
 environment = []
-
 for row in dataset:
     rowlist = []
     for value in row:
@@ -20,46 +19,52 @@ for row in dataset:
     environment.append(rowlist)
 data.close()
 
-matplotlib.pyplot.imshow(environment)
-matplotlib.pyplot.show()
+#Sets up envrioment size variables for use in chart plotter and
+#start location randomiser
+xrange = len(rowlist)-1
+yrange = len(environment)-1
 
 #Number of Agents Variable
-num_of_agents = 100
-num_of_itterations = 1000
+num_of_agents = 10
+num_of_itterations = 100
 
 #Agents List
 agents = []
 
 #Generates a number of agents in random locations
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment))  
-
+    agents.append(agentframework.Agent(random.randint(0,yrange)\
+    , random.randint(0,xrange), environment)) 
+   
 #Moves each agent a number of times
 for j in range(num_of_itterations):
     for i in range(num_of_agents): 
-        agents[i].move()
+        agents[i].move(yrange, xrange)
         agents[i].eat()
-        #agents[i].vomit()
+        agents[i].vomit()
 
-###Plots points, with the most Eastern in Red
-matplotlib.pyplot.ylim(0, 99)
-matplotlib.pyplot.xlim(0, 99)
+###Plots points and envronment in final arranngement
+matplotlib.pyplot.ylim(0, yrange)
+matplotlib.pyplot.xlim(0, xrange)
 matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
 matplotlib.pyplot.show()
 
+#calculates distance between each point
 for agents_row_a in agents:
     for agents_row_b in agents:
         distance = distance_between(agents_row_a, agents_row_b)
-        
+
+#Writes final enviroment to new file
 finalenvironment = open('finalenvironment.csv', 'w', newline = '')
 writer = csv.writer(finalenvironment, delimiter = ',')
 for row in environment:
     writer.writerow(row)
 finalenvironment.close()
 
-
+#Writes agent store values to new file, appending a new line to the file
+#if it already exists on each subsequent exectuion
 agentstore = open('agentstore.csv', 'a', newline = '')
 writestore = csv.writer(agentstore, delimiter=',')
 storelist = []
@@ -69,5 +74,6 @@ writestore.writerow(storelist)
     
 agentstore.close()
 
+#Prints enviroment value at the location of each agent and the store value
 for i in range(num_of_agents):
     print(agents[i], end='\n')
--- agentframework.py
+++ agentframework.py
@@ -1,68 +1,66 @@
 import random
 
-
-class Agent:
-#    def  __init__(self):
-#       self.x = random.randint(0,99)
-#       self.y = random.randint(0,99)
-#       
-#    def move(self):
-#        if random.random() < 0.5:
-#            self.x = (self.x + 1) % 100
-#        else:
-#            self.x = (self.x - 1) % 100
-#        if random.random() < 0.5:
-#            self.y = (self.y + 1) % 100
-#        else:
-#            self.y = (self.y - 1) % 100
-    
-    def  __init__(self, environment, store=0):
-           self._x = random.randint(0,99)
-           self._y = random.randint(0,99)
-           self.environment = environment
-           self.store = 0
+#The Class controlling all agent behviours
+class Agent: 
+    #Initialises instances of Agent class, sets up y,x,environment and store
+    #to be passed from the call
+    def  __init__(self, y, x, environment, store=0):
+        self._x = x
+        self._y = y
+        self.environment = environment
+        self.store = store
            
-       
+    #Sets up x/y properties to conceal _x,_y in from the caller 
     def get_x(self):
         return self._x
-    
     def get_y(self):
-    
         return self._y
-    
     def set_x(self, value):
         self._x = value
-    
     def set_y(self, value):
-        self._y = value
-        
+        self._y = value     
     x = property(get_x, set_x)
     y = property(get_y, set_y)
     
-    def move(self):
+    #Controls agent movement, recieves movement limitation from the call
+    #which controls the boundaries for edge effect handling
+    def move(self, yrange, xrange):
+        yrange = yrange
+        xrange = xrange
         if random.random() < 0.5:
-            self.x = (self.x + 1) % 100
+            self.x = (self.x + 1) % xrange
         else:
-            self.x = (self.x - 1) % 100
+            self.x = (self.x - 1) % xrange
         if random.random() < 0.5:
-            self.y = (self.y + 1) % 100
+            self.y = (self.y + 1) % yrange
         else:
-            self.y = (self.y - 1) % 100
-            
+            self.y = (self.y - 1) % yrange
+    
+    #Controls eating behaviour
     def eat (self):
+        #Default is for agents to eat n units per itteration, adding those
+        #units to it's store and subtractng them from the enviroment location
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10
         else:
+        #Overrides default unit consumtion, if enviroment has less than n units
+        #agent is to eat remaining units. Adding the different amount to agents
+        #store variable and setting the environment variable to 0
             self.store += self.environment[self.y][self.x]
             self.environment[self.y][self.x] = 0
     
+    #Sets up behaviour to test the agent store level, when the store level
+    #exceeds 100 the store is emptied to the environment.
     def vomit(self):
         if self.store > 100:
             self.environment[self.y][self.x] += self.store
             self.store = 0
-            
+    
+    #Overides standard string function, to return the enviroment and store
+    #value for each agent    
     def __str__(self):
-        return "Environment=" + str(self.environment[self.y][self.x]) + " ,store=" + str(self.store)
+        return "Environment=" + str(self.environment[self.y][self.x]) + \
+    " ,Store=" + str(self.store)
     
     
